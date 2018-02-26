789bf5de3fb189a712ec265b171b75bc1a0c6f76
Josh Wilson <joshuef@gmail.com>
2018-02-26 12:15:17 +0100

    setting up for importing by default for safe

--- app/background.process.development.js
+++ app/background.process.development.js
@@ -2,15 +2,60 @@
 
 import logger from 'logger';
 import { configureStore } from 'store/configureStore';
-import { init } from  'extensions/safe/init-safe-background-processes';
+// import { init } from  'extensions/safe/init-safe-background-processes';
 // TODO This handling needs to be imported via extension apis more seemlessly
 import handlePeruseStoreChanges from './peruseSafeApp';
 
+import { isRunningProduction, SAFE } from 'appConstants';
+// import ipc from './ffi/ipc';
+
+import { initAnon, initMock } from 'extensions/safe/network';
+
+
+import * as authAPI from 'extensions/safe/auth-api';
 const initialState = {};
 
 // Add middleware from extensions here. TODO: this should be be unified somewhere.
 const loadMiddlewarePackages = [];
 const store = configureStore( initialState, loadMiddlewarePackages );
+
+const init = async ( store ) =>
+{
+    logger.info( 'Init of bg process.' );
+    // ipc();
+    //
+    // setupWebApis happening via store listeners now... thats it.
+    // setupWebAPIs();
+
+    //TODO: Curerently this is duplicated in BG and netowrk....
+    try
+    {
+        // setup auth
+        authAPI.ffi.ffiLoader.loadLibrary();
+
+        // dont do this inside if auth ffi as circular dep
+        if ( isRunningProduction )
+        {
+            await initAnon( store );
+        }
+        else
+        {
+            await initMock( store );
+        }
+    }
+    catch ( e )
+    {
+        logger.info( 'Problems initing SAFE extension' );
+        logger.info( e.message );
+        logger.info( e );
+    }
+
+    // TODO: Reenable this.
+    // store.subscribe( async () =>
+    // {
+    //     handleMainStoreChanges( store );
+    // } );
+};
 
 init();
 
--- app/setupPreloadAPIs.js
+++ app/setupPreloadAPIs.js
@@ -1,12 +1,12 @@
 // following @pfrazee's beaker pattern again here.
     // import { ipcRenderer } from 'electron';
 // import rpc from 'pauls-electron-rpc';
-// import pkg from 'appPackage';
+import pkg from 'appPackage';
 import logger from 'logger';
-// const VERSION = pkg.version;
-// const WITH_CALLBACK_TYPE_PREFIX = '_with_cb_';
-// const WITH_ASYNC_CALLBACK_TYPE_PREFIX = '_with_async_cb_';
-// const EXPORT_AS_STATIC_OBJ_PREFIX = '_export_as_static_obj_';
+const VERSION = pkg.version;
+const WITH_CALLBACK_TYPE_PREFIX = '_with_cb_';
+const WITH_ASYNC_CALLBACK_TYPE_PREFIX = '_with_async_cb_';
+const EXPORT_AS_STATIC_OBJ_PREFIX = '_export_as_static_obj_';
 
 import * as remoteCallActions from 'actions/remoteCall_actions';
 
@@ -54,70 +54,70 @@ import * as remoteCallActions from 'actions/remoteCall_actions';
 // // this keeps it flexxx.....
 //
 // // method which will populate window with the APIs deemed appropriate for the protocol
-// const setupPreloadAPIs = ( asProtocol ) =>
-// {
-//     // mark the safe protocol as 'secure' to enable all DOM APIs
-//     // webFrame.registerURLSchemeAsSecure('safe');
-//     window[ pkg.name ] = { version: VERSION };
-//     const webAPIs = asProtocol ?
-    //         ipcRenderer.sendSync( 'get-web-api-manifests', asProtocol ) :
-    //         ipcRenderer.sendSync( 'get-web-api-manifests', window.location.protocol ) ;
-//
-//
-//     logger.info( 'WEB APIS ARE HERE>>>>>>>>>', webAPIs );
-//
-//     // create an id to group all safeApp objects
-//     const safeAppGroupId = ( Math.random() * 1000 | 0 ) + Date.now();
-//     window.safeAppGroupId = safeAppGroupId;
-//
-//     Object.keys( webAPIs ).forEach( k =>
-//     {
-//         const fnsToImport = [];
-//         const fnsWithCallback = [];
-//         const fnsWithAsyncCallback = [];
-//         const staticObjs = [];
-//
-//         Object.keys( webAPIs[k] ).forEach( fn =>
-//         {
-//             // We adapt the functions which contain a callback
-//             if ( fn.startsWith( WITH_CALLBACK_TYPE_PREFIX ) )
-//             {
-//                 // We use a readable type to receive the data from the RPC channel
-//                 const manifest = { [fn]: 'readable' };
-//                 const rpcAPI = rpc.importAPI( WITH_CALLBACK_TYPE_PREFIX + k, manifest, { timeout: false } );
-//                 // We expose the function removing the WITH_CALLBACK_TYPE_PREFIX prefix
-//                 const newFnName = fn.replace( WITH_CALLBACK_TYPE_PREFIX, '' );
-//                 fnsWithCallback[newFnName] = readableToCallback( rpcAPI[fn] );
-//             }
-//             else if ( fn.startsWith( WITH_ASYNC_CALLBACK_TYPE_PREFIX ) )
-//             {
-//                 // We use a readable type to receive the data from the RPC channel
-//                 const manifest = { [fn]: 'readable' };
-//                 const rpcAPI = rpc.importAPI( WITH_ASYNC_CALLBACK_TYPE_PREFIX + k, manifest, { timeout: false } );
-//                 // We expose the function removing the WITH_ASYNC_CALLBACK_TYPE_PREFIX prefix
-//                 const newFnName = fn.replace( WITH_ASYNC_CALLBACK_TYPE_PREFIX, '' );
-//                 // Provide the safeAppGroupId to map it to all safeApp instances created,
-//                 // so they can be automatically freed when the page is closed or refreshed
-//                 fnsWithAsyncCallback[newFnName] = readableToAsyncCallback( rpcAPI[fn], safeAppGroupId );
-//             }
-//             else if ( fn.startsWith( EXPORT_AS_STATIC_OBJ_PREFIX ) )
-//             {
-//                 const manifest = { [fn]: 'sync' };
-//                 const rpcAPI = rpc.importAPI( EXPORT_AS_STATIC_OBJ_PREFIX + k, manifest, { timeout: false } );
-//                 // We expose the object name removing the EXPORT_AS_STATIC_OBJ_PREFIX prefix
-//                 const objName = fn.replace( EXPORT_AS_STATIC_OBJ_PREFIX, '' );
-//                 // Call the function to expose just the returned value
-//                 staticObjs[objName] = rpcAPI[fn].call();
-//             }
-//             else
-//             {
-//                 fnsToImport[fn] = webAPIs[k][fn];
-//             }
-//         });
-//
-//         window[k] = Object.assign( rpc.importAPI( k, fnsToImport, { timeout: false } ), staticObjs, fnsWithCallback, fnsWithAsyncCallback );
-//     } );
-// }
+export const setupPreloadedSafeAPIs = ( store, hasAuth ) =>
+{
+    // mark the safe protocol as 'secure' to enable all DOM APIs
+    // webFrame.registerURLSchemeAsSecure('safe');
+    window[ pkg.name ] = { version: VERSION };
+    const webAPIs = asProtocol ?
+            ipcRenderer.sendSync( 'get-web-api-manifests', asProtocol ) :
+            ipcRenderer.sendSync( 'get-web-api-manifests', window.location.protocol ) ;
+
+
+    logger.info( 'WEB APIS ARE HERE>>>>>>>>>', webAPIs );
+
+    // create an id to group all safeApp objects
+    const safeAppGroupId = ( Math.random() * 1000 | 0 ) + Date.now();
+    window.safeAppGroupId = safeAppGroupId;
+
+    Object.keys( webAPIs ).forEach( k =>
+    {
+        const fnsToImport = [];
+        const fnsWithCallback = [];
+        const fnsWithAsyncCallback = [];
+        const staticObjs = [];
+
+        Object.keys( webAPIs[k] ).forEach( fn =>
+        {
+            // We adapt the functions which contain a callback
+            if ( fn.startsWith( WITH_CALLBACK_TYPE_PREFIX ) )
+            {
+                // We use a readable type to receive the data from the RPC channel
+                const manifest = { [fn]: 'readable' };
+                const rpcAPI = rpc.importAPI( WITH_CALLBACK_TYPE_PREFIX + k, manifest, { timeout: false } );
+                // We expose the function removing the WITH_CALLBACK_TYPE_PREFIX prefix
+                const newFnName = fn.replace( WITH_CALLBACK_TYPE_PREFIX, '' );
+                fnsWithCallback[newFnName] = readableToCallback( rpcAPI[fn] );
+            }
+            else if ( fn.startsWith( WITH_ASYNC_CALLBACK_TYPE_PREFIX ) )
+            {
+                // We use a readable type to receive the data from the RPC channel
+                const manifest = { [fn]: 'readable' };
+                const rpcAPI = rpc.importAPI( WITH_ASYNC_CALLBACK_TYPE_PREFIX + k, manifest, { timeout: false } );
+                // We expose the function removing the WITH_ASYNC_CALLBACK_TYPE_PREFIX prefix
+                const newFnName = fn.replace( WITH_ASYNC_CALLBACK_TYPE_PREFIX, '' );
+                // Provide the safeAppGroupId to map it to all safeApp instances created,
+                // so they can be automatically freed when the page is closed or refreshed
+                fnsWithAsyncCallback[newFnName] = readableToAsyncCallback( rpcAPI[fn], safeAppGroupId );
+            }
+            else if ( fn.startsWith( EXPORT_AS_STATIC_OBJ_PREFIX ) )
+            {
+                const manifest = { [fn]: 'sync' };
+                const rpcAPI = rpc.importAPI( EXPORT_AS_STATIC_OBJ_PREFIX + k, manifest, { timeout: false } );
+                // We expose the object name removing the EXPORT_AS_STATIC_OBJ_PREFIX prefix
+                const objName = fn.replace( EXPORT_AS_STATIC_OBJ_PREFIX, '' );
+                // Call the function to expose just the returned value
+                staticObjs[objName] = rpcAPI[fn].call();
+            }
+            else
+            {
+                fnsToImport[fn] = webAPIs[k][fn];
+            }
+        });
+
+        window[k] = Object.assign( rpc.importAPI( k, fnsToImport, { timeout: false } ), staticObjs, fnsWithCallback, fnsWithAsyncCallback );
+    } );
+}
 
 const setupPreloadAPIs = ( store ) =>
 {
